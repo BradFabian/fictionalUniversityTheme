08fad68ad5ffdae70f2f58f9ce29918c5e53af27
Brad Fabian <bradleofabian@gmail.com>
2020-11-30 12:11:42 -0500

    set up theme

---
+++ wp-content/themes/fictionalUniversity-theme/single.php
@@ -0,0 +1,11 @@
+<?php get_header(); 
+
+
+if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+<h2><?php the_title(); ?></h2>
+<?php the_content(); ?>
+<?php endwhile; else : ?>
+	<p><?php esc_html_e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; 
+
+get_footer(); ?>
